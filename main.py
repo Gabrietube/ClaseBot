720ac541cf6fa37a8ef3b784a307dfa8fca6e80d
Gabrietube <136658584+Gabrietube@users.noreply.github.com>
2023-07-08 10:46:24 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run("MTEyNjMxNjczNTkzMTM2NzUzNA.GpBcK5.sluniyuAztGbv6GnLR3nA1ITvbhjsZkXOLfWj0")
